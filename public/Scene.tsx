6ab0a566c84e7944a85a6680c50d434b51fd5845
Harshgup16 <hkgupta160420@gmail.com>
2025-07-06 12:20:38 +0530

    Update Scene.tsx

--- public/Scene.tsx
+++ public/Scene.tsx
@@ -137,52 +137,52 @@
 // }
 
 
-"use client"
+// "use client"
 
-import * as THREE from "three"
-import { Canvas, useThree } from "@react-three/fiber"
-import Model from "./Model"
-import { Suspense } from "react"
-import { useProgress, Html, ScrollControls, OrbitControls, Environment, Lightformer } from "@react-three/drei"
+// import * as THREE from "three"
+// import { Canvas, useThree } from "@react-three/fiber"
+// import Model from "./Model"
+// import { Suspense } from "react"
+// import { useProgress, Html, ScrollControls, OrbitControls, Environment, Lightformer } from "@react-three/drei"
 
-import { LUTCubeLoader, ToneMappingMode } from 'postprocessing'
-import { EffectComposer, Bloom, LUT, BrightnessContrast, HueSaturation, ToneMapping } from '@react-three/postprocessing'
-function Loader() {
-  const { progress, active } = useProgress()
+// import { LUTCubeLoader, ToneMappingMode } from 'postprocessing'
+// import { EffectComposer, Bloom, LUT, BrightnessContrast, HueSaturation, ToneMapping } from '@react-three/postprocessing'
+// function Loader() {
+//   const { progress, active } = useProgress()
 
-  return <Html center>{progress.toFixed(1)} % loaded</Html>
-}
+//   return <Html center>{progress.toFixed(1)} % loaded</Html>
+// }
 
-export default function Scene() {
-  return (
-    <Canvas gl={{ antialias: true }} className="relative h-svh" style={{ background: "black" }}
-      camera={{ position: [0, 2.5, 5], fov: 50 }}
-      onCreated={(state) => {
-        state.gl.toneMapping = THREE.NoToneMapping
-      }}
-    >
-      {/* <color attach="background" args={['#151520']} /> */}
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <directionalLight position={[-5, -5, 5]} intensity={4} />
-      <Environment files={"/blue_photo_studio_1k.hdr"} >
-        <group rotation={[0, 0, 1]}>
-          <Lightformer form="circle" intensity={10} position={[0, 10, -10]} scale={20} onUpdate={(self) => self.lookAt(0, 0, 0)} />
-          <Lightformer intensity={0.1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[-5, 1, -1]} rotation-y={Math.PI / 2} scale={[50, 10, 1]} />
-          <Lightformer intensity={0.1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 1, 0]} rotation-y={-Math.PI / 2} scale={[50, 10, 1]} />
-          <Lightformer color="white" intensity={0.2} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[0, 1, 0]} scale={[10, 100, 1]} />
-        </group>
-      </Environment>
-      <EffectComposer enableNormalPass>
-        <Bloom mipmapBlur luminanceThreshold={1} intensity={2} />
-        {/* <BrightnessContrast brightness={0} contrast={0.1} /> */}
-        {/* <HueSaturation hue={0} saturation={-0.25} /> */}
-      </EffectComposer>
-      <Suspense fallback={<Loader />}>
-          <OrbitControls />
-          <Model />
-      </Suspense>
-    </Canvas>
-  )
-}
+// export default function Scene() {
+//   return (
+//     <Canvas gl={{ antialias: true }} className="relative h-svh" style={{ background: "black" }}
+//       camera={{ position: [0, 2.5, 5], fov: 50 }}
+//       onCreated={(state) => {
+//         state.gl.toneMapping = THREE.NoToneMapping
+//       }}
+//     >
+//       {/* <color attach="background" args={['#151520']} /> */}
+//       <ambientLight intensity={0.5} />
+//       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+//       <pointLight position={[-10, -10, -10]} />
+//       <directionalLight position={[-5, -5, 5]} intensity={4} />
+//       <Environment files={"/blue_photo_studio_1k.hdr"} >
+//         <group rotation={[0, 0, 1]}>
+//           <Lightformer form="circle" intensity={10} position={[0, 10, -10]} scale={20} onUpdate={(self) => self.lookAt(0, 0, 0)} />
+//           <Lightformer intensity={0.1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[-5, 1, -1]} rotation-y={Math.PI / 2} scale={[50, 10, 1]} />
+//           <Lightformer intensity={0.1} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 1, 0]} rotation-y={-Math.PI / 2} scale={[50, 10, 1]} />
+//           <Lightformer color="white" intensity={0.2} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[0, 1, 0]} scale={[10, 100, 1]} />
+//         </group>
+//       </Environment>
+//       <EffectComposer enableNormalPass>
+//         <Bloom mipmapBlur luminanceThreshold={1} intensity={2} />
+//         {/* <BrightnessContrast brightness={0} contrast={0.1} /> */}
+//         {/* <HueSaturation hue={0} saturation={-0.25} /> */}
+//       </EffectComposer>
+//       <Suspense fallback={<Loader />}>
+//           <OrbitControls />
+//           <Model />
+//       </Suspense>
+//     </Canvas>
+//   )
+// }
